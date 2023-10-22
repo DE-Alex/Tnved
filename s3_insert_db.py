6e6ab6dbb47fb11dd4b09b7a17ba9bdb7c600e1a
DE-Alex <139635578+DE-Alex@users.noreply.github.com>
2023-10-22 09:35:05 +0300

    Update s3_insert_db.py

--- s3_insert_db.py
+++ s3_insert_db.py
@@ -6,66 +6,88 @@ import glob
 from pathlib import Path
 
 # developed modules
-import s6_db_operations
 import s5_common_func
-import sql.create_objects
+import s6_db_operations
+from sql.stage_create_objects import create_scheme
+from sql.stage_create_objects import create_tables
 
 # read configs
 config = configparser.ConfigParser() 
 config.read(Path(sys.path[0], 'pipeline.conf'))
 
 tmp_folder = Path(sys.path[0], config['general']['tmp_folder'])
-sch_name = config['stage_layer']['scheme_name']
+db_type = config['general']['db_type']
+stage_scheme = config['stage_layer']['scheme_name']
+
+file_name = config['sqlite']['sqlite_stage_file']
+sqlite_stage_path = Path(sys.path[0], file_name)
+
 
 def main():
-    s6_db_operations.drop_scheme(sch_name)#@@@@@@@@@@
- 
-    # create tables if not exist  
-    conn = s6_db_operations.connect_to_db()    
-    for query in sql.create_objects.actions:
-        print(query)
-        curs = conn.cursor()
+    # search .csv files in temp folder
+    f_paths = s5_common_func.search_by_mask(tmp_folder, '*.csv')     
+    if len(f_paths) == 0:
+        print('No *.csv files with data found. Run s1 and s2 again.')
+        return None
+
+    # @@@@@remove after testing@@@@@
+    if db_type == 'sqlite':
+        conn = s6_db_operations.connect_to_db(sqlite_stage_path)
+        pass
+    elif db_type == 'postgres':
+        conn = s6_db_operations.connect_to_db()
+        a = input(f'Drop scheme "{stage_scheme}"? (y)')
+        if a == 'y':
+            s6_db_operations.drop_scheme(conn, stage_scheme)
+            print(f'scheme {stage_scheme} droped')
+            # create scheme 
+            curs = conn.cursor()
+            for query in create_scheme:
+                curs.execute(query)
+            curs.close()
+            print(f'scheme {stage_scheme} created')
+    # @@@@@remove after testing@@@@@            
+            
+    # check db tables and create if not exist 
+    curs = conn.cursor()    
+    for query in create_tables:
+        #curs = conn.cursor()
         curs.execute(query)
-        conn.commit()
-        print('ok')
-    conn.close()
-    msg = f'Create tables (if not exist) - ok'
+    curs.close()
+    conn.commit()
+    #conn.close()
+    msg = f'"CREATE <table> IF NOT EXIST" - ok'
     print(msg)
     s5_common_func.write_journal(msg)
-
-    # select table names from db
-    table_names = s6_db_operations.select_table_names(sch_name)
-    
-    # truncate tables with data if already exist
-    for tb_name in table_names:
-        s6_db_operations.truncate_table(sch_name, tb_name)
-        msg = f'TRUNCATE {tb_name} - ok'
-        print(msg)
-        s5_common_func.write_journal(msg)
- 
-    # search .csv files in temp folder
-    f_paths = s5_common_func.search_by_mask(tmp_folder, '*.csv')
-    if len(f_paths) == 0:
-        print('\n - *.csv fles with data not found. \n - Extract and process them again with "s1" and "s2" scripts.')
-    
+  
     # read data from .csv files
     for f_path in f_paths:
+        # get short filename (without extension)
+        short_name = Path(f_path).stem
+        
+        # get table name by short filename
+        table_name = config['stage_layer'][short_name]
+        
+        if db_type == 'postgres':
+            table_name = f'{stage_scheme}.{table_name}'
+        
+        # truncate table to clean it if data exists
+        s6_db_operations.truncate_table(conn, table_name)    
+    
         with open(f_path, newline='') as csv_file:
             dt_reader = csv.reader(csv_file, delimiter = '|')
             dataset = list(dt_reader)
-        
-        # get short filename (without extension)
-        short_name = Path(f_path).stem
-        # get table name by short filename
-        table_name = sch_name + '.' + config['stage_layer'][short_name]
-        
+      
         # get column names from table
-        columns = s6_db_operations.select_col_names(table_name)
+        columns = s6_db_operations.select_col_names(conn, table_name)
         # insert dataset into table
-        s6_db_operations.insert(table_name, columns, dataset)
+        s6_db_operations.insert(conn, table_name, columns, dataset)
                 
         # delete .csv files to clean up tmp folder
         os.remove(f_path)
-
+    
+    conn.close()
+    
 if __name__ == '__main__':
     main()
+    
