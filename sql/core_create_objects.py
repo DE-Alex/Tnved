c70a5922eede3fcd234162e0e46ba0df7c233cab
DE-Alex <139635578+DE-Alex@users.noreply.github.com>
2023-10-22 09:35:34 +0300

    Update core_create_objects.py

--- sql/core_create_objects.py
+++ sql/core_create_objects.py
@@ -1,79 +1,92 @@
-actions = [  
-            '''
-            CREATE SCHEMA IF NOT EXISTS tn_ved_relative;
-            '''
-            # ,
-            # '''
-            # CREATE TABLE IF NOT EXISTS tn_ved_relative.tb_periods (
-                # period_id SERIAL NOT NULL PRIMARY KEY,
-                # time_from varchar NOT NULL,
-                # expired2 varchar NULL             
-            # );
-            # '''
-            ,
-            '''
-            CREATE TABLE IF NOT EXISTS tn_ved_relative.tb_tnved_version (
-                table_name varchar NOT NULL,
-                version varchar NOT NULL,
-                date_from varchar NOT NULL,
-                some_code varchar NULL,
-                period_id int2 NOT NULL ,
-                time_from varchar NOT NULL,
-                expired2 varchar NULL,
-                CONSTRAINT tb_tnved_version_pkey PRIMARY KEY (table_name, version, period_id)
-            );
-            '''
-            ,
-            '''
-            CREATE TABLE IF NOT EXISTS tn_ved_relative.tb_razdel (
-                razdel int2 UNIQUE NOT NULL,
-                naim varchar NULL,
-                prim varchar NULL,
-                date_from varchar NOT NULL,
-                expired varchar NULL,
-                CONSTRAINT tb_razdel_pkey PRIMARY KEY (razdel, date_from)
-            );
-            '''
-            ,
-            '''
-            CREATE TABLE IF NOT EXISTS tn_ved_relative.tb_gruppa (
-                razdel int2 NULL,
-                gruppa int2 UNIQUE NOT NULL,
-                naim varchar NULL,
-                prim varchar NULL,
-                date_from varchar NOT NULL,
-                expired varchar NULL,
-                FOREIGN KEY (razdel)  REFERENCES tn_ved_relative.tb_razdel (razdel),
-                CONSTRAINT tb_gruppa_pkey PRIMARY KEY (gruppa, date_from)
-            );            
-            '''
-            ,
-            '''
-            CREATE TABLE IF NOT EXISTS tn_ved_relative.tb_tov_poz (
-                gruppa int2 NOT NULL,
-                tov_poz int2 UNIQUE NOT NULL,
-                naim varchar NULL,
-                date_from varchar NOT NULL,
-                expired varchar NULL,
-                FOREIGN KEY (gruppa)  REFERENCES tn_ved_relative.tb_gruppa (gruppa),
-                CONSTRAINT tb_tov_poz_pkey PRIMARY KEY (gruppa, tov_poz, date_from)
-            );             
-            '''
-            ,
-            '''
-            CREATE TABLE IF NOT EXISTS tn_ved_relative.tb_sub_poz (
-                gruppa int2 NOT NULL,
-                tov_poz int2 NOT NULL,
-                sub_poz int4 NOT NULL,
-                kr_naim varchar NULL,
-                date_from varchar NOT NULL,
-                expired varchar NULL,
-                FOREIGN KEY (gruppa)  REFERENCES tn_ved_relative.tb_gruppa (gruppa),
-                FOREIGN KEY (tov_poz)  REFERENCES tn_ved_relative.tb_tov_poz (tov_poz),
-                CONSTRAINT tb_sub_poz_pkey PRIMARY KEY (gruppa, tov_poz, sub_poz, date_from)
-            );
-            '''
-         ]
+import sys
+import configparser
+from pathlib import Path
+# read configs
+config = configparser.ConfigParser() 
+config.read(Path(sys.path[0], 'pipeline.conf'))
+
+core_scheme = config['core_layer']['scheme_name']
+# table names in stage scheme and core scheme are equal
+tb_razdel_name = config['stage_layer']['tb_razdel_name']
+tb_gruppa_name = config['stage_layer']['tb_gruppa_name']
+tb_tov_poz_name = config['stage_layer']['tb_tov_poz_name']
+tb_sub_poz_name = config['stage_layer']['tb_sub_poz_name']
+tb_version_name = config['stage_layer']['tb_version_name']
+
+create_scheme =  [
+                '''
+                CREATE SCHEMA IF NOT EXISTS {0};
+                '''.format(core_scheme)
+                ,
+                '''
+                SET search_path TO {0};
+                '''.format(core_scheme)
+                ]
+
+create_tables = [  
+
+                '''
+                CREATE TABLE IF NOT EXISTS {0} (
+                    razdel int2 NOT NULL,
+                    naim varchar NULL,
+                    prim varchar NULL,
+                    date_from varchar NOT NULL,
+                    expired varchar NULL,
+                    CONSTRAINT {0}_pkey PRIMARY KEY (razdel, date_from)
+                );
+                '''.format(tb_razdel_name)
+                ,
+                '''
+                CREATE TABLE IF NOT EXISTS {0} (
+                    razdel int2 NOT NULL,
+                    gruppa int2 NOT NULL,
+                    naim varchar NULL,
+                    prim varchar NULL,
+                    date_from varchar NOT NULL,
+                    expired varchar NULL,
+                    FOREIGN KEY (razdel, date_from)  REFERENCES {1} (razdel, date_from),
+                    CONSTRAINT {0}_pkey PRIMARY KEY (razdel, gruppa, date_from)
+                );            
+                '''.format(tb_gruppa_name,tb_razdel_name )
+                ,
+                '''
+                CREATE TABLE IF NOT EXISTS {0} (
+                    razdel int2 NOT NULL,                
+                    gruppa int2 NOT NULL,
+                    tov_poz int2 NOT NULL,
+                    naim varchar NULL,
+                    date_from varchar NOT NULL,
+                    expired varchar NULL,
+                    FOREIGN KEY (razdel, gruppa, date_from)  REFERENCES {1} (razdel, gruppa, date_from),
+                    CONSTRAINT {0}_pkey PRIMARY KEY (razdel, gruppa, tov_poz, date_from)
+                );             
+                '''.format(tb_tov_poz_name, tb_gruppa_name)
+                ,
+                '''
+                CREATE TABLE IF NOT EXISTS {0} (
+                    razdel int2 NOT NULL,                
+                    gruppa int2 NOT NULL,
+                    tov_poz int2 NOT NULL,
+                    sub_poz int4 NOT NULL,
+                    kr_naim varchar NULL,
+                    date_from varchar NOT NULL,
+                    expired varchar NOT NULL,
+                    FOREIGN KEY (razdel, gruppa, tov_poz, date_from)  REFERENCES {1} (razdel, gruppa, tov_poz, date_from),
+                    CONSTRAINT {0}_pkey PRIMARY KEY (razdel, gruppa, tov_poz, sub_poz, date_from, expired)
+                );
+                '''.format(tb_sub_poz_name, tb_tov_poz_name)
+                ,
+                                '''
+                CREATE TABLE IF NOT EXISTS {0} (
+                    file_name varchar NOT NULL,
+                    table_name varchar NOT NULL,
+                    version varchar NOT NULL,
+                    date_from varchar NOT NULL,
+                    some_code varchar NULL,
+                    CONSTRAINT {0}_pkey PRIMARY KEY (table_name, version)
+                );
+                '''.format(tb_version_name)
+                ]
          
 if __name__ == '__main__': 
 	pass
