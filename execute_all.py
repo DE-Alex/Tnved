4a091585650554a5583de9c736655a3e6176eb08
DE-Alex <139635578+DE-Alex@users.noreply.github.com>
2023-10-22 09:24:42 +0300

    add s4 script

--- execute_all.py
+++ execute_all.py
@@ -1,10 +1,12 @@
 import s1_extract
 import s2_normalize
 import s3_insert_db
+import s4_data_model
 
 s1_extract.main()
 s2_normalize.main()
 s3_insert_db.main()
+s4_data_model.main()
           
 if __name__ == '__main__':
    pass
