babcd51b5742ae3fb1495d418247f5340cb2ba33
DE-Alex <139635578+DE-Alex@users.noreply.github.com>
2023-10-22 09:35:24 +0300

    Update s6_db_operations.py

--- s6_db_operations.py
+++ s6_db_operations.py
@@ -1,6 +1,7 @@
 import sys
 import configparser
 import psycopg
+import sqlite3
 from pathlib import Path
 from math import ceil
 
@@ -10,67 +11,88 @@ import s5_common_func
 config = configparser.ConfigParser() 
 config.read(Path(sys.path[0], 'pipeline.conf'))
 
+db_type = config['general']['db_type']
+
 dbname = config['postgres']['database']
 user = config['postgres']['username']
 password = config['postgres']['password']
 host = config['postgres']['host']
 port = config['postgres']['port']
         
-def connect_to_db():
+def connect_to_db(sqlight_path = None):
     try:
-        conn = psycopg.connect(
-            'dbname=' + dbname
-            + ' user=' + user
-            + ' password=' + password
-            + ' host=' + host,
-            port = port)
+        if db_type == 'postgres':
+            conn = psycopg.connect(
+                'dbname=' + dbname
+                + ' user=' + user
+                + ' password=' + password
+                + ' host=' + host,
+                port = port)
+                
+        elif db_type == 'sqlite':
+            conn = sqlite3.connect(sqlight_path)
+        else:
+            print(f'unknown db_type "{db_type}"')
+        
     except psycopg.OperationalError as e:
         print('psycopg.OperationalError:', e)
-        return exit(1)
+        conn.close()            
+        return exit(1)        
+    except sqlite3.DatabaseError as err:
+        print('Error: ', err)
+        conn.close()
+        return exit(1) 
     return conn
 
-def drop_scheme(sch_name):
+def drop_scheme(conn, sch_name):
     query ='''
             DROP SCHEMA {0} CASCADE;
             '''.format(sch_name)
-    a = input(f'Drop scheme "{sch_name}"? (y)')
-    if a == 'y':
-        conn = connect_to_db()
-        curs = conn.cursor()
-        curs.execute(query)         
-        conn.close()    
+    #conn = connect_to_db()
+    curs = conn.cursor()
+    curs.execute(query) 
+    #?conn.commit()    
+    curs.close()
+    #conn.close()  
     
-def select_table_names(sch_name):
+def select_table_names(conn, sch_name):
     query ='''
             SELECT tablename FROM pg_tables 
             WHERE schemaname = '{0}'
             '''.format(sch_name)
-    conn = connect_to_db()
+    #conn = connect_to_db()
     curs = conn.cursor()
     curs.execute(query)            
     result = curs.execute(query).fetchall()
-    conn.close()   
+    curs.close()    
+    #conn.close()   
     table_names = [item[0] for item in result]
     return(table_names)  
 
-def select_col_names(table_name):
+def select_col_names(conn, table_name):
     query = f'SELECT * FROM {table_name} LIMIT 0;'
-    conn = connect_to_db()
+    #conn = connect_to_db(sqlite_stage_file)
     curs = conn.cursor()
     curs.execute(query)
     columns = [desc[0] for desc in curs.description]
-    conn.close()
+    curs.close()    
+    #conn.close()
     return columns 
 
-def truncate_table(sch_name, table_name):
-    query = f'TRUNCATE {sch_name}.{table_name} CASCADE;'
-    conn = connect_to_db()
+def truncate_table(conn, table_name):
+    if db_type == 'postgres':
+        query = f'TRUNCATE {table_name} CASCADE;'
+    elif db_type == 'sqlite':
+        query = f'DELETE FROM {table_name};'
+    print(query)
+    #conn = connect_to_db(sqlite_stage_file)
     curs = conn.cursor()
     curs.execute(query)
+    curs.close()        
     conn.commit()
-    conn.close()
+    #conn.close()
         
-def insert(table_name, columns, dataset):
+def insert(conn, table_name, columns, dataset):
     # divide dataset to insert by parts
     data_parts = split_dataset(dataset)
 
@@ -78,35 +100,47 @@ def insert(table_name, columns, dataset):
     print(msg, end = '')
     s5_common_func.write_journal(msg) 
     
-    conn = connect_to_db()
+    #conn = connect_to_db(sqlite_stage_file)
+    curs = conn.cursor()
     ins_total = 0
     for part in data_parts:
         try:
             for row in part:
-                with conn.cursor() as curs:
+                if db_type == 'postgres':
+                    # form set of values, replace '' cells with None
+                    row = [None if cell == '' else cell for cell in row]
+                    
                     placeholders = (', ').join(['%s']*len(columns))
                     
                     # "quote" columns to escape lowercase by PostgreSQL
-                    cols_str = (', ').join([f'"{name}"' for name in columns])
+                    cols_str = (', ').join([f'"{name}"' for name in columns]) 
                     
-                    # form set of values in order by columns
-                    values = [None if cell == '' else cell for cell in row]
-
                     query = f'INSERT INTO {table_name} ({cols_str}) VALUES ({placeholders});'
-                    curs.execute(query, values)
+                    curs.execute(query, row)
+                elif db_type == 'sqlite':
+                    # form set of values, replace None cells with ''
+                    row = ['' if cell == None else cell for cell in row]                
+                    data_values = (', ').join(f'"{cell}"' for cell in row)
+                    cols_str = (', ').join(columns) 
+                    query = f'INSERT INTO {table_name} ({cols_str}) VALUES ({data_values});'
+                    curs.execute(query)
             conn.commit()
         except psycopg.Error as e:
-            pass
-            #print(query)
-            #print(values)
-            #print(e)
-            #conn.rollback()
-            #conn.close()
-        
+            print('query:', query)
+            print('values:', row)
+            print(e)
+            conn.rollback()
+        except sqlite3.DatabaseError as e:
+            print('query:', query)
+            print('values:', row)
+            print(e)
+            conn.rollback()          
+   
         N = len(part)
         print(f'{N} ', sep=' ', end='', flush=True)
         ins_total = ins_total + N
-    conn.close()
+    curs.close()
+    #conn.close()
     print(f'\nTotal: {ins_total} rows\n')
     
 def split_dataset(dataset):
