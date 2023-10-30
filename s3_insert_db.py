33c34d6c8bffb254ea4e3fa0dd965351fbe2c389
DE-Alex <139635578+DE-Alex@users.noreply.github.com>
2023-10-30 19:46:22 +0300

    replace pure SQL with Pandas + SQLAlchemy

--- s3_insert_db.py
+++ s3_insert_db.py
@@ -1,14 +1,13 @@
 # standart modules
 import configparser
 import sys, os
-import csv
-import glob
+import pandas as pd
+from sqlalchemy import create_engine
+from sqlalchemy import MetaData
 from pathlib import Path
 
 # developed modules
 import s5_common_func
-import s6_db_operations
-from sql.stage_create_objects import create_scheme
 from sql.stage_create_objects import create_tables
 
 # read configs
@@ -17,11 +16,17 @@ config.read(Path(sys.path[0], 'pipeline.conf'))
 
 tmp_folder = Path(sys.path[0], config['general']['tmp_folder'])
 db_type = config['general']['db_type']
+
+#SQLight settings
+sqlite_stage_path = Path(sys.path[0], config['sqlite']['sqlite_stage_file'])
+
+#Postgres settings
+user = config['postgres']['username']
+password = config['postgres']['password']
+host = config['postgres']['host']
+port = config['postgres']['port']
+dbname = config['postgres']['database']
 stage_scheme = config['stage_layer']['scheme_name']
-
-file_name = config['sqlite']['sqlite_stage_file']
-sqlite_stage_path = Path(sys.path[0], file_name)
-
 
 def main():
     # search .csv files in temp folder
@@ -30,37 +35,29 @@ def main():
         print('No *.csv files with data found. Run s1 and s2 again.')
         return None
 
-    # @@@@@remove after testing@@@@@
+    # create SQLAlchemy engine
     if db_type == 'sqlite':
-        conn = s6_db_operations.connect_to_db(sqlite_stage_path)
-        pass
+        engine = create_engine(f'sqlite:///{sqlite_stage_path}')
+        db_schema = None
     elif db_type == 'postgres':
-        conn = s6_db_operations.connect_to_db()
-        a = input(f'Drop scheme "{stage_scheme}"? (y)')
-        if a == 'y':
-            s6_db_operations.drop_scheme(conn, stage_scheme)
-            print(f'scheme {stage_scheme} droped')
-            # create scheme 
-            curs = conn.cursor()
-            for query in create_scheme:
-                curs.execute(query)
-            curs.close()
-            print(f'scheme {stage_scheme} created')
-    # @@@@@remove after testing@@@@@            
-            
-    # check db tables and create if not exist 
-    curs = conn.cursor()    
-    for query in create_tables:
-        #curs = conn.cursor()
-        curs.execute(query)
-    curs.close()
-    conn.commit()
-    #conn.close()
-    msg = f'"CREATE <table> IF NOT EXIST" - ok'
-    print(msg)
-    s5_common_func.write_journal(msg)
-  
-    # read data from .csv files
+        engine = create_engine(f'postgresql+psycopg://{user}:{password}@{host}:{port}/{dbname}')
+        db_schema = stage_scheme
+    conn = engine.connect()
+    md_obj = MetaData(db_schema)    
+    
+    a = input(f'Drop scheme "{stage_scheme}"? (y)')
+    if a == 'y':
+        md_obj.reflect(conn)
+        md_obj.drop_all(conn)
+        md_obj = MetaData(db_schema)
+        md_obj = create_tables(md_obj)
+        md_obj.create_all(conn)  
+        conn.commit()
+        msg = f'scheme {stage_scheme} droped. tables created.'
+        print(msg)
+        s5_common_func.write_journal(msg)
+    md_obj.reflect(conn)
+    
     for f_path in f_paths:
         # get short filename (without extension)
         short_name = Path(f_path).stem
@@ -68,26 +65,25 @@ def main():
         # get table name by short filename
         table_name = config['stage_layer'][short_name]
         
-        if db_type == 'postgres':
-            table_name = f'{stage_scheme}.{table_name}'
+        # get column names  
+        if db_type == 'sqlite':
+            col_names = md_obj.tables[table_name].columns.keys()
+        elif db_type == 'postgres':
+            col_names = md_obj.tables[stage_scheme + '.' + table_name].columns.keys()
         
-        # truncate table to clean it if data exists
-        s6_db_operations.truncate_table(conn, table_name)    
-    
-        with open(f_path, newline='') as csv_file:
-            dt_reader = csv.reader(csv_file, delimiter = '|')
-            dataset = list(dt_reader)
+        # read data from .csv files
+        dataset = pd.read_csv(f_path, delimiter = '|', header = None, names = col_names, encoding = 'utf-8')
+        
+        # insert dataset into database table        
+        result = dataset.to_sql(table_name, conn, db_schema, if_exists = 'replace', index = False, chunksize = 1000)
+        conn.commit()
+        msg = f'{table_name}: inserted'
+        print(msg)
+        s5_common_func.write_journal(msg)        
       
-        # get column names from table
-        columns = s6_db_operations.select_col_names(conn, table_name)
-        # insert dataset into table
-        s6_db_operations.insert(conn, table_name, columns, dataset)
-                
         # delete .csv files to clean up tmp folder
         os.remove(f_path)
-    
     conn.close()
     
 if __name__ == '__main__':
     main()
-    
