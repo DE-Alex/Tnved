ec1291c9439e6e026be8130b9d847cfc8bd3186c
DE-Alex <139635578+DE-Alex@users.noreply.github.com>
2023-11-01 18:52:17 +0300

    Update s2_normalize.py

--- s2_normalize.py
+++ s2_normalize.py
@@ -78,7 +78,7 @@ def main():
             dt_writer.writerows(dataset)
         
         # delete .txt files to clean up tmp folder
-        #os.remove(txt_path)
+        os.remove(txt_path)
         
     msg = f'decode and normalize - ok'
     print(msg)
